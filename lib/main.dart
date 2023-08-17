f90a84a05fb2ce23377a016230868990d6cb162d
Aarthikootharasan <91037876+Aarthikootharasan@users.noreply.github.com>
2023-08-17 18:11:56 +0530

    organized flutter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/Dashboard.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'My flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashbaord'.toUpperCase()),
-          backgroundColor: Colors.deepOrange,
-        ),
-        body: Center(
-          child: Text.rich(
-            TextSpan(
-              text: 'My',
-              children: [
-                TextSpan(
-                    text: 'Flutter',
-                    style:
-                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-                TextSpan(
-                    text: 'App',
-                    style: TextStyle(fontSize: 50.0, color: Colors.blue)),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Dashboard()
+    );
+  }
 }
